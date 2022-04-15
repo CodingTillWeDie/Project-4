658920839499ba1251ea29c8c4ac262beea4085c
Kevin Kim <98494454+kevink0908@users.noreply.github.com>
2022-04-14 17:26:37 -0700

    Update MaxHeap.java

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,7 +1,6 @@
-public final class MaxHeap<T extends Comparable<? super T>>
-        implements MaxHeapInterface<T>
+public final class MaxHeap implements MaxHeapInterface
 {
-    private T[] heap; // Array of heap entries
+    private int[] heap; // Array of heap entries
     private int lastIndex; // Index of last entry
     private boolean initialized = false;
     private static int numSwaps = 0;
@@ -18,34 +17,36 @@ public final class MaxHeap<T extends Comparable<? super T>>
         if (initialCapacity < DEFAULT_CAPACITY)
             initialCapacity = DEFAULT_CAPACITY;
         else
-            checkCapacity(initialCapacity);
+            checkCapacity(initialCapacity + 1);
 
-        // The cast is safe because the new array contains all null entries.
-        @SuppressWarnings("unchecked")
-        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        int[] tempHeap = new int[initialCapacity + 1];
         heap = tempHeap;
         lastIndex = 0;
         initialized = true;
     } // end of type constructor #1
 
-    public MaxHeap(T[] entries)
+    public MaxHeap(int[] entries)
     {
         this(entries.length); // call the other type constructor.
         assert initialized = true;
 
         // copy given array to data field.
         for (int index = 0; index < entries.length; index++)
+        {
             heap[index + 1] = entries[index];
+        }
 
         // create a max-heap.
         for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
+        {
             reheap(rootIndex);
+        }
     } // end of type constructor #2
 
-    private void reheap(int rootIndex)
+    public void reheap(int rootIndex)
     {
         boolean done = false;
-        T orphan = heap[rootIndex];
+        int orphan = heap[rootIndex];
         int leftChildIndex = 2 * rootIndex;
 
         while(!done && (leftChildIndex <= lastIndex))
@@ -53,15 +54,15 @@ public final class MaxHeap<T extends Comparable<? super T>>
             int largerChildIndex = leftChildIndex; // assume that left child is larger.
             int rightChildIndex = leftChildIndex + 1;
             if ((rightChildIndex <= lastIndex)
-                    && (heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0))
+                    && (heap[rightChildIndex] > heap[largerChildIndex]))
             {
                 largerChildIndex = rightChildIndex; // right child is larger than left child.
             } // end if
 
-            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            if (orphan < heap[largerChildIndex])
             {
                 heap[rootIndex] = heap[largerChildIndex];
-                ++numSwaps;
+                numSwaps++;
                 rootIndex = largerChildIndex;
                 leftChildIndex = 2 * rootIndex;
             }
@@ -72,16 +73,17 @@ public final class MaxHeap<T extends Comparable<? super T>>
         heap[rootIndex] = orphan;
     } // end of reheap
 
-    public void add(T newEntry)
+    public void add(int newEntry)
     {
         checkInitialization(); // ensure initialization of data fields.
         int newIndex = lastIndex + 1;
         int parentIndex = newIndex / 2;
 
         // perform up-heap operation in order to add an entry.
-        while ((parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
+        while ((parentIndex > 0) && newEntry > heap[parentIndex])
         {
             heap[newIndex] = heap[parentIndex];
+            numSwaps++;
             newIndex = parentIndex;
             parentIndex = newIndex / 2;
         } // end while
@@ -91,26 +93,26 @@ public final class MaxHeap<T extends Comparable<? super T>>
         ensureCapacity();
     } // end of add
 
-    public T removeMax()
+    public int removeMax()
     {
         checkInitialization(); // ensure initialization of data fields.
-        T root = null;
+        int root = -1;
 
         if (!isEmpty())
         {
             root = heap[1]; // return value
-            heap[1] = heap[lastIndex]; // form a semiheap
-            lastIndex--; // decrease size
-            reheap(1); // Transform to a heap
+            heap[1] = heap[lastIndex]; // form a semi-heap.
+            lastIndex--; // decrease size.
+            reheap(1); // Transform to a heap.
         } // end if
 
         return root;
     } // end of removeMax
 
-    public T getMax()
+    public int getMax()
     {
         checkInitialization(); // ensure initialization of data fields.
-        T root = null;
+        int root = -1;
 
         if (!isEmpty())
             root = heap[1];
@@ -137,14 +139,15 @@ public final class MaxHeap<T extends Comparable<? super T>>
     {
         checkInitialization(); // ensure initialization of data fields.
 
-        while(lastIndex > -1)
-        {
-            heap[lastIndex] = null;
-            lastIndex--;
-        } // end while
+        heap = null;
         lastIndex = 0;
 
     } // end of clear
+
+    public int[] getHeap()
+    {
+        return heap;
+    }
 
     private void checkCapacity(int initialCapacity)
     {
